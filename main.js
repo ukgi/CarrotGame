e71bae4159d4d5af2c85e9a5c5726ec9563e8742
chanuk <doosan1224@naver.com>
2023-06-01 21:40:12 +0900

    [Feat] : 게임 시작 시, 당근과 벌레 구현

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const gameBtn = document.querySelector(".gameBtn");
+const header = document.querySelector("header");
+const gameSection = document.querySelector(".gameSection");
 const counter = document.querySelector(".counter");
 const carrotCounter = document.querySelector(".carrotCounter");
 const textBox = document.querySelector(".textBox");
 const stopBtn = gameBtn.querySelector(".fa-stop");
 
 let carrotNumber = 12;
+let bugNumber = 12;
 let isStart = false;
 let timer;
 
@@ -28,6 +31,7 @@ textBox.addEventListener("click", (e) => {
 });
 
 function startGame() {
+  resetGame();
   isStart = true;
   // 1. 시작 버튼의 모양을 중지 버튼으로 바꾼다
   const playIcon = gameBtn.querySelector(".fa-play");
@@ -38,31 +42,37 @@ function startGame() {
 
   // 3. 당근의 갯수가 보인다
   carrotCounter.textContent = `${carrotNumber}`;
+
+  // 4. 당근이랑 벌레를 만든다
+  makeCarrots();
+  makeBugs();
 }
 
 function stopGame(isGameIng) {
   // 1. 게임 중에 중단 버튼을 누르면
+  isStart = false;
+  stopCount(timer);
+  textBox.style.display = "block";
+
+  header.style.pointerEvents = "none";
+  gameSection.style.pointerEvents = "none";
+
   if (isGameIng === true) {
-    stopCount(timer);
     textBox.innerHTML = `
     <h1>다시 시작하겠습니까?</h1>
     <button class="retryBtn">
         <i class="fas fa-redo"></i>
     </button>
     `;
-    textBox.style.display = "block";
   }
   // 2. 타이머가 다 되서 게임이 종료되면
   else {
-    isStart = false;
-    stopCount(timer);
     textBox.innerHTML = `
         <h1>실패 😖</h1>
         <button class="retryBtn">
             <i class="fas fa-redo"></i>
         </button>
         `;
-    textBox.style.display = "block";
   }
 }
 
@@ -80,4 +90,60 @@ function startCount() {
 
 function stopCount(timer) {
   clearInterval(timer);
+}
+
+function makeCarrots() {
+  const gameSectionX = gameSection.getBoundingClientRect().x;
+  const gameSectionRight = gameSection.getBoundingClientRect().right;
+
+  const gameSectionY = gameSection.getBoundingClientRect().y;
+  const gameSectionBottom = gameSection.getBoundingClientRect().bottom;
+
+  for (let i = 0; i < carrotNumber; i++) {
+    const carrotX = random(gameSectionX, gameSectionRight);
+    const carrotY = random(gameSectionY, gameSectionBottom);
+
+    const carrot = document.createElement("img");
+    carrot.setAttribute("src", "./assets/img/carrot.png");
+    carrot.style.position = "absolute";
+
+    const carrotTop = carrotY - gameSectionY;
+    const carrotLeft = carrotX - gameSectionX;
+    carrot.style.top = `${carrotTop}px`;
+    carrot.style.left = `${carrotLeft}px`;
+
+    gameSection.appendChild(carrot);
+  }
+}
+
+function makeBugs() {
+  const gameSectionX = gameSection.getBoundingClientRect().x;
+  const gameSectionRight = gameSection.getBoundingClientRect().right;
+
+  const gameSectionY = gameSection.getBoundingClientRect().y;
+  const gameSectionBottom = gameSection.getBoundingClientRect().bottom;
+
+  for (let i = 0; i < bugNumber; i++) {
+    const bugX = random(gameSectionX, gameSectionRight);
+    const bugY = random(gameSectionY, gameSectionBottom);
+
+    const bug = document.createElement("img");
+    bug.setAttribute("src", "./assets/img/bug.png");
+    bug.style.position = "absolute";
+
+    const bugTop = bugY - gameSectionY;
+    const bugLeft = bugX - gameSectionX;
+    bug.style.top = `${bugTop}px`;
+    bug.style.left = `${bugLeft}px`;
+
+    gameSection.appendChild(bug);
+  }
+}
+
+function resetGame() {
+  gameSection.replaceChildren();
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
